6d79fe4bc21146882a9b3fff8df51fc284efcd6a
Maria Escribano <maescrib@eu.westcongrp.com>
2018-03-01 16:00:02 +0100

    Init06

--- skills/command-help.js
+++ skills/command-help.js
@@ -8,6 +8,7 @@ module.exports = function (controller) {
         text += "\n- " + bot.enrichCommand(message, ".commons") + ": shows metadata about myself";
         text += "\n- " + bot.enrichCommand(message, "help") + ": spreads the word about my skills";
         text += "\n- " + bot.enrichCommand(message, "espacio") + ": copying the room";
+        text += "\n- " + bot.enrichCommand(message, "add") + ": add people";
         bot.reply(message, text);
     });
 }
